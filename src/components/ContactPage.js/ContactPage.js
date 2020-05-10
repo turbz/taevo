de875fc36d243e5bcd1a2e7faaf1124783eaeb1c
thabisoddx <thabisomoha@gmail.com>
2020-05-10 12:25:02 +0200

    setting nodemailer

--- src/components/ContactPage.js/ContactPage.js
+++ src/components/ContactPage.js/ContactPage.js
@@ -15,7 +15,7 @@ export default function ContactPage() {
     const message = document.getElementById("message").value;
     axios({
       method: "POST",
-      url: "https://turbz.github.io/taevo-website/",
+      url: "https://turbz.github.io/taevo-website/send",
       data: {
         name: name,
         email: email,
