2ce32e42a8e1a80d13e6703c941836d61c7bc9e4
thabisoddx <thabisomoha@gmail.com>
2020-05-12 22:22:56 +0200

    adding image slider

--- src/components/AppBanner/AppBanner.js
+++ src/components/AppBanner/AppBanner.js
@@ -6,12 +6,20 @@ import html from "../assets/user-friendly-icon.svg";
 import report from "../assets/customer-icon.svg";
 import password from "../assets/password.svg";
 import reporting from "../assets/reporting-icon.svg";
+import Slider from "./Slider/Slider";
+
+const images = [
+  "https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
+  "https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80",
+  "https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80",
+  "https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80",
+];
 
 export default function AppBanner() {
   return (
     <section className="App-Banner">
       <div>
-        <div>
+        <div className="content">
           <h4>
             <span></span>
             <span>Get Work Done</span>
@@ -38,6 +46,7 @@ export default function AppBanner() {
             </li>
           </ul>
         </div>
+        <Slider slides={images} />
       </div>
       <div id="features">
         <h3>
---
+++ src/components/AppBanner/Slider/Slider.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+
+import "./Slider.css";
+
+// Desktop Banners
+
+import banner1 from "../../assets/bg-banner.png";
+import banner2 from "../../assets/bg-banner-2.png";
+
+// Movile banners
+
+// import ipad1 from "../../assets/bg-ipad.png";
+// import ipad2 from "../../assets/bg-ipad-2.png";
+
+export default function Slider() {
+  const [slide, setSlide] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSlide((slide) => slide + 1);
+
+      if (slide === 2) {
+        setSlide(1);
+      }
+    }, 8000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [slide]);
+
+  return (
+    <>
+      <div className="Slider desktop">
+        {slide === 1 && (
+          <img
+            src={banner1}
+            alt="slide"
+            className=" animate__animated animate__bounce slower "
+          />
+        )}
+        {slide === 2 && (
+          <img
+            src={banner2}
+            alt="slide"
+            className=" animate__animated animate__slideInRight faster"
+          />
+        )}
+
+        <ul>
+          <li onClick={() => setSlide(1)}></li>
+          <li onClick={() => setSlide(2)}></li>
+        </ul>
+      </div>
+
+      {/* <div className="Slider mobile">
+        {slide === 1 && (
+          <img
+            src={ipad1}
+            alt="slide"
+            className="animate__animated animate__bounce slower "
+          />
+        )}
+        {slide === 2 && (
+          <img
+            src={ipad2}
+            alt="slide"
+            className="animate__animated animate__slideInRight faster"
+          />
+        )}
+        <ul>
+          <li onClick={() => setSlide(1)}></li>
+          <li onClick={() => setSlide(2)}></li>
+        </ul>
+      </div> */}
+    </>
+  );
+}
