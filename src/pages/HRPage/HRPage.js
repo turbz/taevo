3825086080bd4057ec7697c3c7ecc8a5048096b2
thabisoddx <thabisomoha@gmail.com>
2020-05-12 15:16:39 +0200

    adding CNAME file

---
+++ src/pages/HRPage/HRPage.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import "./HRPage.css";
+
+export default function HRPage() {
+  return (
+    <section className="HR-Page">
+      <div className="banner">
+        <footer>
+          <h4>Story #1</h4>
+          <h1>Define a Headcount Management</h1>
+          <p>
+            Find exactly who you need, when you need them. Utilize our headcount
+            management tool for both onboarding and offboarding employees. Get
+            comprehensive data insight about each employee/department based on
+            demographics, department spending, employee job history, etc.
+          </p>
+        </footer>
+      </div>
+      <div className="content">
+        <h2>
+          Provide trustworthy data to complete day-to-day task more efficient.
+        </h2>
+        <p>
+          {" "}
+          Find exactly who you need, when you need them. Utilize our headcount
+          management tool for both onboarding and offboarding employees. Get
+          comprehensive data insight about each employee/department based on
+          demographics, department spending, employee job history, etc.
+        </p>
+        <ul>
+          <li>Transform your employee experience.</li>
+          <li>Transform your employee experience.</li>
+          <li>Transform your employee experience.</li>
+          <li>Transform your employee experience.</li>
+        </ul>
+        <p>
+          Talent has never been more important to the success of a business.{" "}
+        </p>
+        <p>
+          Talent has never been more important to the success of a business.{" "}
+        </p>
+      </div>
+    </section>
+  );
+}
