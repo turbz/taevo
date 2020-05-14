f36a664783d20c7337a8a7e324b43a92bb56154f
thabisoddx <thabisomoha@gmail.com>
2020-05-14 13:03:10 +0200

    Updates

---
+++ precache-manifest.e5f82b45139441a32143232c4e773427.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93056722424713b35d3a1c8b89303558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f0e34283aa851e73f07",
+    "url": "/static/css/2.1d3a4d29.chunk.css"
+  },
+  {
+    "revision": "63bf6923873386e6c52d",
+    "url": "/static/css/main.39792c26.chunk.css"
+  },
+  {
+    "revision": "1f0e34283aa851e73f07",
+    "url": "/static/js/2.405c42d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.405c42d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63bf6923873386e6c52d",
+    "url": "/static/js/main.9dcc20e8.chunk.js"
+  },
+  {
+    "revision": "77c21fdf50ad613a02d2",
+    "url": "/static/js/runtime-main.7686a62c.js"
+  },
+  {
+    "revision": "3383aedd24d5bef3bebee5d76949d294",
+    "url": "/static/media/bg-banner-2.3383aedd.png"
+  },
+  {
+    "revision": "307eba01b9a83cde7f7eab3833d35e4b",
+    "url": "/static/media/bg-banner-3.307eba01.png"
+  },
+  {
+    "revision": "4716c2367b5ee191b4c09790b78531cd",
+    "url": "/static/media/bg-banner.4716c236.png"
+  },
+  {
+    "revision": "24453d4ffe1c83e0c8e69044b1218008",
+    "url": "/static/media/bg-ipad-nav.24453d4f.png"
+  },
+  {
+    "revision": "2463846a0db122589fed3a6ce27598ab",
+    "url": "/static/media/bg-ipad.2463846a.png"
+  },
+  {
+    "revision": "7445865b7d0b75cfe8530bf1b7937cc2",
+    "url": "/static/media/bg-services.7445865b.svg"
+  },
+  {
+    "revision": "9177f07116a90dce0af46d6434eeff3a",
+    "url": "/static/media/customer-icon.9177f071.svg"
+  },
+  {
+    "revision": "3d129382121e65977c838aae6999aabf",
+    "url": "/static/media/exit-white.3d129382.svg"
+  },
+  {
+    "revision": "3727299debb312d977c39afa4de0048a",
+    "url": "/static/media/exit.3727299d.svg"
+  },
+  {
+    "revision": "97de0aa6eb9d584aac00695c3cfc7a95",
+    "url": "/static/media/facebook.97de0aa6.svg"
+  },
+  {
+    "revision": "5e7add39530df5f2fe78681efdcdd30b",
+    "url": "/static/media/hr-atoms.5e7add39.png"
+  },
+  {
+    "revision": "ca769da41145ad96d5b13555afea8437",
+    "url": "/static/media/imageReporting.ca769da4.png"
+  },
+  {
+    "revision": "db540427fb6a8c2fecc4f01ef0843009",
+    "url": "/static/media/imageTraining.db540427.png"
+  },
+  {
+    "revision": "33b14d8ea82efafc63441daca12e45a7",
+    "url": "/static/media/job.33b14d8e.svg"
+  },
+  {
+    "revision": "0bf2b9a5d2e0cfe0d21f02dc5a2dabf0",
+    "url": "/static/media/linkedin-icon.0bf2b9a5.svg"
+  },
+  {
+    "revision": "69096082dbfea1eb537e2d28c58a0b57",
+    "url": "/static/media/logo-full-color.69096082.svg"
+  },
+  {
+    "revision": "ec5bad2e6e297056d27ad614cd0523d8",
+    "url": "/static/media/menu-icon.ec5bad2e.svg"
+  },
+  {
+    "revision": "dfda738a9968d6803ef87c8ddfdf7638",
+    "url": "/static/media/office.dfda738a.svg"
+  },
+  {
+    "revision": "667e8ccb78b6c752863ab2deb0536d6b",
+    "url": "/static/media/password.667e8ccb.svg"
+  },
+  {
+    "revision": "f0e84765b113501a8060236f583d1398",
+    "url": "/static/media/recruiting-atoms.f0e84765.png"
+  },
+  {
+    "revision": "aa137f8ab2aa53f48d45cc1196bea56b",
+    "url": "/static/media/reporting-icon.aa137f8a.svg"
+  },
+  {
+    "revision": "86b77da616f7cee4aa07e33a1c60a094",
+    "url": "/static/media/smartphone.86b77da6.svg"
+  },
+  {
+    "revision": "3d03ad2f59eacb0de9143afc1f946a19",
+    "url": "/static/media/tick.3d03ad2f.svg"
+  },
+  {
+    "revision": "7303a9587042115530d2bb28f8f1a574",
+    "url": "/static/media/twitter.7303a958.svg"
+  },
+  {
+    "revision": "a9c207171aa669b8e42f497e86bf0d41",
+    "url": "/static/media/user-friendly-icon.a9c20717.svg"
+  },
+  {
+    "revision": "d7c926fa0d87febd40babb4731f7db07",
+    "url": "/static/media/work.d7c926fa.svg"
+  }
+]);
